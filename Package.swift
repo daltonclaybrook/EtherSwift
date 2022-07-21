08ea51c456110ff07317b63f034756196230d86d
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-07-20 23:39:02 -0400

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -1,28 +1,25 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "EtherSwift",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "EtherSwift",
-            targets: ["EtherSwift"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "EtherSwift",
-            dependencies: []),
-        .testTarget(
-            name: "EtherSwiftTests",
-            dependencies: ["EtherSwift"]),
-    ]
+	name: "EtherSwift",
+	products: [
+		.library(name: "EtherSwift", targets: ["EtherSwift"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+		.package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.5.1")
+	],
+	targets: [
+		.target(name: "EtherSwift", dependencies: [
+			"BigInt",
+			"CryptoSwift"
+		]),
+		.testTarget(name: "EtherSwiftTests", dependencies: [
+			"EtherSwift",
+			"BigInt",
+			"CryptoSwift"
+		])
+	]
 )
